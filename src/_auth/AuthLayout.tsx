173480402d21a7e0e9691c7219c3858d70a4b119
Gajendra <gajendranh1092@gmail.com>
2024-12-21 13:23:15 +0530

    Added images and icons

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -11,6 +11,8 @@ function AuthLayout() {
           <Outlet />
         </section>
         
+
+        <img src="/assets/images/side-img.svg" alt="logo" className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'/>
         </>
        )}
     </>
