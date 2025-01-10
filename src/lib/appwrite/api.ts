f409a4047ec5736aafd1b3e4bcd29492e1676309
Gajendra <gajendranh1092@gmail.com>
2025-01-11 00:17:02 +0530

    add createUserAccount function to handle user account creation

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,60 @@
+import {ID} from 'appwrite';
+import {account, appwriteconfig, avatars, databases} from '@/lib/appwrite/config';
+import { INewUser } from "@/types";
+import { string } from 'zod';
+
+export async function createUserAccount(user: INewUser ) {
+       
+    try{
+
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name
+        )
+
+        if(!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(user.name);
+
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl : avatarUrl,
+        })
+        return newUser;
+    }
+
+    catch(error){
+        console.log(error);
+        return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+
+    accountId: string;
+    email:string;
+    name:string;
+    imageUrl:URL;
+    username?:string;
+}){
+
+    try {
+        const newUser = await databases.createDocument(
+            appwriteconfig.databaseId,
+            appwriteconfig.userCollectionId,
+            ID.unique(),
+            user,
+        )
+
+        return newUser;
+    }
+    catch(error){
+            console.log(error);
+        }
+    
+}
